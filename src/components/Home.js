fb1065ed8c2012e38c95751eef5e3a02958f74c9
Maddy0102 <madhanraj.0199@gmail.com>
2024-01-26 17:13:05 +0530

    home changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,67 +4,38 @@ import exploreImage from '../images/explore.jpg';
 import symbol7Image from '../images/Symble7.jpg';
 import whitecard from '../images/whitecard.avif'
 const Home = () => {
-  const [currentSlide, setCurrentSlide] = useState(1);
+  const [currentSlide, setCurrentSlide] = useState(0);
 
   useEffect(() => {
     const showSlides = () => {
-      let slides = document.querySelectorAll(".mySlides");
-      let dots = document.querySelectorAll(".dot");
 
-      if (slides.length === 0 || dots.length === 0) {
-        // Elements not found, handle error or return
-        return;
-      }
-
-      if (currentSlide > slides.length) setCurrentSlide(1);
-      if (currentSlide < 1) setCurrentSlide(slides.length);
-
-      for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-      }
-
-      for (let i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-      }
-
-      slides[currentSlide - 1].style.display = "block";
-      dots[currentSlide - 1].className += " active";
     };
 
     showSlides();
   }, [currentSlide]);
 
   const plusSlides = (n) => {
-    setCurrentSlide((prev) => prev + n);
+    setCurrentSlide((prev) => {
+      return prev + n >= 0 ? (prev + n)%(slides.length) : slides.length - 1
+    });
   };
 
   const currentSlideHandler = (n) => {
     setCurrentSlide(n);
   };
 
+  const [slides, setSlides] = useState([
+    { id: 0, title: "Welcome To Brandlet", image: exploreImage },
+    { id: 1, title: "We are a creative group of people who design influential brands and digital experiences", image: whitecard },
+    { id: 2, image: symbol7Image },
+  ])  
+
   return (
     <div className="slideshow-container">
       {/* Full-width images with number and caption text */}
-      <div className="mySlides fade">
-        <img src={exploreImage} alt="Explore" style={{ width: '100%' }} />
-        {/* Button and text inside the image */}
-          <div className="image-overlay">
-            <h1 className="overlay-text-h1">Welcome To Brandlet</h1>
-              <button className="overlay-button" onClick={() => console.log('Button Clicked')}>
-                Explore
-              </button>
-          </div>
-      </div>
-
-      <div className="mySlides fade">
-        <img src={whitecard} alt="whitecard" style={{ width: '100%' }} />
-          <div className="image-overlay">
-            <p className="overlay-text-p">We are a creative group of people who design influential brands and digital experiences</p>
-          </div>
-      </div>
-
-      <div className="mySlides fade">
-        <img src={symbol7Image} alt="Symbol 7" style={{ width: '100%' }} />
+      <div className="mySlides fade" style={{ position: 'relative' }}>
+        <img src={slides[currentSlide].image} style={{ width: '100%' }} />
+        <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>{slides[currentSlide].title}</div>
       </div>
 
       {/* Next and previous buttons */}
@@ -77,9 +48,7 @@ const Home = () => {
 
       {/* The dots/circles */}
       <div style={{ textAlign: 'center' }}>
-        <span className="dot" onClick={() => currentSlideHandler(1)}></span>
-        <span className="dot" onClick={() => currentSlideHandler(2)}></span>
-        <span className="dot" onClick={() => currentSlideHandler(3)}></span>
+        {slides.map((slide) => (<span key={slide.id} className="dot" onClick={() => currentSlideHandler(slide.id)}></span>))}
       </div>
     </div>
   );
